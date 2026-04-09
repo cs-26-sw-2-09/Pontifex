67b17046dcebd8f45978eae5627c4a0c8cbdaee5
Krog <Rasmkrog@gmail.com>
2026-04-09 12:57:33 +0200

    linting

--- src/routes/api/user/getStudents/+server.ts
+++ src/routes/api/user/getStudents/+server.ts
@@ -6,14 +6,16 @@
 //initial commit
 
 export async function GET() {
-    // Insert of test Data,
-    const { Users } = await import("$lib/index");
-    
-    // Filters through users who has the student role, then maps the Id to an array of numbers
-    const students: number[] = Users.filter(user => user.Role === "Student").map(student => student.Id);
-    
-    // Retuns Json object including student users ID  
-    return new Response(JSON.stringify(students), {
-        headers: { "Content-Type": "application/json" }
-    });
+	// Insert of test Data,
+	const { Users } = await import("$lib/index");
+
+	// Filters through users who has the student role, then maps the Id to an array of numbers
+	const students: number[] = Users.filter((user) => user.Role === "Student").map(
+		(student) => student.Id
+	);
+
+	// Retuns Json object including student users ID
+	return new Response(JSON.stringify(students), {
+		headers: { "Content-Type": "application/json" }
+	});
 }
