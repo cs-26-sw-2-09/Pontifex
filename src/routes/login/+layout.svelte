80b68f45ebd6aafe1809cf7ffe9eaf0972652931
sankethshanmuganathan <sankethshan19@gmail.com>
2026-04-13 13:19:26 +0200

    svelte something something

---
+++ src/routes/login/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	// Should have a function to set authN cookie to browser
+	//
+	// There should be 3 buttons, one for teacher login, one for student login and one for admin login
+	// When a button is pressed, it should fetch all users of said type, the type should be a param
+	// All results should be shown in a dropdown
+	// After a user selects the account, pinky authN is engaged
+	// Then set browser cookie with user id
+	// Redirect to new page
+	//
+</script>
