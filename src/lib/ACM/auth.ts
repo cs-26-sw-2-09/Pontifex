77eef390c67ee23f0cf1b72196c7d6d9220a6647
Krog <Rasmkrog@gmail.com>
2026-04-14 13:59:59 +0200

    Added comments for acm function

--- src/lib/ACM/auth.ts
+++ src/lib/ACM/auth.ts
@@ -1,11 +1,47 @@
-import { Action, type User, type Resource, type Relations, Role } from "$lib/types.js";
+import {Action, type User, type Resource, type Relations, Role } from "$lib/types.js";
+import {db} from "$lib/server/db/index.js"
+import { User } from '../types';
+import { PgSchema } from "drizzle-orm/pg-core";
+import { permission } from "process";
 
-export function hasAccess(user: User, resource: Resource, relation : Relations, action : Action): boolean {
+export function hasAccess(user: User, resource: Resource, relation : Relations, action : Action, sessionId = 1/* Session ID  (Cookie): String*/): boolean {
+	
+	//Compare Session ID from web browser with User ID from Current User
+	if(user.Id !== sessionId) { return false }
+	
 	// Check if the user and the requested resource belong to the same school if not return false
 	if (Number(user.Attributes.get("SchoolID")) !== resource.SchoolID) {
 		return false;
 	}
+
+	//if role is admin return true
 	if (user.Role === Role.Admin) { return true; }
+
+	/*
+	If resource.type === skema && resource.ownerID == user.ID  return true;
+
+	*/
+
+
+	// Get user Permisions from DataBase
+	// db.select().from(schema.user).where(eq(schema.user.Name, "John Doe"))
+	// needs changing
+	// Access Database, select from (table) where feild user id === current user.Id
+	// permisions [] = db.select().from(PgSchema.permisions()).where(eq(PgSchema.user.Id, user.Id))
+	
+	//validate if permisions is empty
+	//if(permisions undefined)
+	// permission ?? return false;
+	
+	// Compare Action With usersepermissions
+	
+	// Filter on Action for array
+	// actionPermList = permisions.filter(action)
+	// resourcePermList = actionPermList.filter("resource.resourceType")
+
+	// if(resourcePermList.filter(resource.resourceId).value()) {return true;}
+	// else {return false}
+
 
 	switch( resource.type ){
 		// All sub authorization modules and functions.
