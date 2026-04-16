44875af5f96d76ccf3c0793a09d2004529d71059
sankethshanmuganathan <sankethshan19@gmail.com>
2026-04-16 10:59:28 +0200

    Lidt farver

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,12 @@
   import { goto } from '$app/navigation';
 </script>
 
-<h1>Welcome to Lectio</h1>
-<h2>Choose your role to continue</h2>
+<div class="min-h-screen bg-gray-400 flex flex-col items-center justify-start p-48 text-center">
+  <h1 class="text-5xl font-bold">Lectio Pontifex</h1>
+  <h2 class="text-xl mt-4">Choose your role to continue</h2>
+
 <button on:click={() => goto('/student')}>Student</button>
 <button on:click={() => goto('/teacher')}>Teacher</button>
 <button on:click={() => goto('/admin')}>Admin</button>
+
+  </div>
