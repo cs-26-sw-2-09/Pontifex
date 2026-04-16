18fbec05cbae2e42c4df99f39531a2dc32f849fe
Krog <Rasmkrog@gmail.com>
2026-04-16 13:32:01 +0200

    Redundant lines have been removed

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,11 +2,7 @@
 // for information about these interfaces
 declare global {
 	namespace App {
-		interface Error {
-			code: string;
-			id;
-			string;
-		}
+		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
