8a62184b31d1247456d0865c0d565ea9b55176f3
Nicolai Holmgaard <Nicolai@Holmgaard.info>
2026-04-17 13:31:56 +0200

    and a comma

--- src/routes/teacher/+page.svelte
+++ src/routes/teacher/+page.svelte
@@ -1,4 +1,4 @@
-<h1 class="text-center text-[42px]"> Teacher frontpage</h1>
+<h1 class="text-center text-[42px]">Teacher frontpage</h1>
 
 <div class="m-10 grid grid-cols-3 content-start gap-4">
 	<div class="box-border h-50 w-full rounded-lg border-2 bg-gray-200 p-3 shadow-lg">
@@ -63,4 +63,3 @@
 		</ul>
 	</div>
 </div>
-
