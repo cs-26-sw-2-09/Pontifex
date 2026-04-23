46747eb2f8af1157987adffa1f4e5ba31023d041
Nicolai Holmgaard <Nicolai@Holmgaard.info>
2026-04-23 15:54:35 +0200

    Changed test to check from null to undefined

--- src/routes/api/user/getStudents/+server.ts
+++ src/routes/api/user/getStudents/+server.ts
@@ -9,17 +9,17 @@ import { GetUsersWithRole } from "$lib/server/db";
 import { Role } from "$lib/types";
 
 export async function GET() {
-  // Insert of test Data,
-  //const { Users } = await import("$lib/index");
+	// Insert of test Data,
+	//const { Users } = await import("$lib/index");
 
-  // Filters through users who has the student role, then maps the Id to an array of numbers
-  //const students: number[] = Users.filter((user) => user.Role === "Student").map(
-  //	(student) => student.Id
-  //);
-  const students = await GetUsersWithRole(Role.Student);
+	// Filters through users who has the student role, then maps the Id to an array of numbers
+	//const students: number[] = Users.filter((user) => user.Role === "Student").map(
+	//	(student) => student.Id
+	//);
+	const students = await GetUsersWithRole(Role.Student);
 
-  // Retuns Json object including student users ID
-  return new Response(JSON.stringify(students), {
-    headers: { "Content-Type": "application/json" }
-  });
+	// Retuns Json object including student users ID
+	return new Response(JSON.stringify(students), {
+		headers: { "Content-Type": "application/json" }
+	});
 }
--- src/test/db.test.ts
+++ src/test/db.test.ts
@@ -3,158 +3,158 @@ import { GetUserFromId, GetUsersWithRole } from "$lib/server/db";
 import { Role } from "$lib/types";
 
 describe("Get user with user info", () => {
-  it("User id of 1 should give Big John", async () => {
-    expect(await GetUserFromId(1, true)).toStrictEqual({
-      Id: 1,
-      Name: "Big John",
-      Role: "Admin",
-      UserInfo: [
-        {
-          Id: 1,
-          UserId: 1,
-          Gender: "Other",
-          Email: "Big@John.com",
-          PhoneNumber: "12345678",
-          Birthdate: "0001-01-01",
-          CPR: "0101010000",
-          Address: "In the mine"
-        }
-      ],
-      Course: []
-      //"HandedInAssignments": []
-    });
-  });
+	it("User id of 1 should give Big John", async () => {
+		expect(await GetUserFromId(1, true)).toStrictEqual({
+			Id: 1,
+			Name: "Big John",
+			Role: "Admin",
+			UserInfo: [
+				{
+					Id: 1,
+					UserId: 1,
+					Gender: "Other",
+					Email: "Big@John.com",
+					PhoneNumber: "12345678",
+					Birthdate: "0001-01-01",
+					CPR: "0101010000",
+					Address: "In the mine"
+				}
+			],
+			Course: []
+			//"HandedInAssignments": []
+		});
+	});
 });
 
 describe("Get user without user info", () => {
-  it("User id of 5 should give John Pork", async () => {
-    expect(await GetUserFromId(5)).toStrictEqual({
-      Id: 5,
-      Name: "John Pork",
-      Role: "Student",
-      Course: [
-        {
-          Id: 1,
-          Name: "English",
-          Description: null
-        }
-      ]
-    });
-  });
+	it("User id of 5 should give John Pork", async () => {
+		expect(await GetUserFromId(5)).toStrictEqual({
+			Id: 5,
+			Name: "John Pork",
+			Role: "Student",
+			Course: [
+				{
+					Id: 1,
+					Name: "English",
+					Description: null
+				}
+			]
+		});
+	});
 });
 
 describe("Get all users with role of Student", () => {
-  it("Should return all students", async () => {
-    expect(await GetUsersWithRole(Role.Student)).toStrictEqual([
-      {
-        Id: 4,
-        Name: "Bob the builder",
-        Role: "Student",
-        Course: [
-          {
-            Id: 1,
-            Name: "English",
-            Description: null
-          }
-        ]
-      },
-      {
-        Id: 5,
-        Name: "John Pork",
-        Role: "Student",
-        Course: [
-          {
-            Id: 1,
-            Name: "English",
-            Description: null
-          }
-        ]
-      },
-      {
-        Id: 7,
-        Name: "James Doakes",
-        Role: "Student",
-        Course: []
-      },
-      {
-        Id: 8,
-        Name: "Big Chungus",
-        Role: "Student",
-        Course: []
-      },
-      {
-        Id: 9,
-        Name: "Charlie Kirk",
-        Role: "Student",
-        Course: []
-      },
-      {
-        Id: 11,
-        Name: "Maga Morten",
-        Role: "Student",
-        Course: []
-      },
-      {
-        Id: 12,
-        Name: "Tobias Kaa",
-        Role: "Student",
-        Course: []
-      },
-      {
-        Id: 13,
-        Name: "Labre Larve",
-        Role: "Student",
-        Course: []
-      },
-      {
-        Id: 14,
-        Name: "Dorthe Donut",
-        Role: "Student",
-        Course: []
-      },
-      {
-        Id: 15,
-        Name: "King Tutankhamon",
-        Role: "Student",
-        Course: []
-      }
-    ]);
-  });
+	it("Should return all students", async () => {
+		expect(await GetUsersWithRole(Role.Student)).toStrictEqual([
+			{
+				Id: 4,
+				Name: "Bob the builder",
+				Role: "Student",
+				Course: [
+					{
+						Id: 1,
+						Name: "English",
+						Description: null
+					}
+				]
+			},
+			{
+				Id: 5,
+				Name: "John Pork",
+				Role: "Student",
+				Course: [
+					{
+						Id: 1,
+						Name: "English",
+						Description: null
+					}
+				]
+			},
+			{
+				Id: 7,
+				Name: "James Doakes",
+				Role: "Student",
+				Course: []
+			},
+			{
+				Id: 8,
+				Name: "Big Chungus",
+				Role: "Student",
+				Course: []
+			},
+			{
+				Id: 9,
+				Name: "Charlie Kirk",
+				Role: "Student",
+				Course: []
+			},
+			{
+				Id: 11,
+				Name: "Maga Morten",
+				Role: "Student",
+				Course: []
+			},
+			{
+				Id: 12,
+				Name: "Tobias Kaa",
+				Role: "Student",
+				Course: []
+			},
+			{
+				Id: 13,
+				Name: "Labre Larve",
+				Role: "Student",
+				Course: []
+			},
+			{
+				Id: 14,
+				Name: "Dorthe Donut",
+				Role: "Student",
+				Course: []
+			},
+			{
+				Id: 15,
+				Name: "King Tutankhamon",
+				Role: "Student",
+				Course: []
+			}
+		]);
+	});
 });
 
 describe("Get all users with role of Teacher", () => {
-  it("Should return all teachers", async () => {
-    expect(await GetUsersWithRole(Role.Teacher)).toStrictEqual([
-      {
-        Id: 3,
-        Name: "Caleb Hane",
-        Role: "Teacher",
-        Course: []
-      },
-      {
-        Id: 6,
-        Name: "Hanne Pande",
-        Role: "Teacher",
-        Course: []
-      },
-      {
-        Id: 10,
-        Name: "Ole Larsen",
-        Role: "Teacher",
-        Course: [
-          {
-            Id: 2,
-            Name: "Math",
-            Description: null
-          }
-        ]
-      }
-    ]);
-  });
+	it("Should return all teachers", async () => {
+		expect(await GetUsersWithRole(Role.Teacher)).toStrictEqual([
+			{
+				Id: 3,
+				Name: "Caleb Hane",
+				Role: "Teacher",
+				Course: []
+			},
+			{
+				Id: 6,
+				Name: "Hanne Pande",
+				Role: "Teacher",
+				Course: []
+			},
+			{
+				Id: 10,
+				Name: "Ole Larsen",
+				Role: "Teacher",
+				Course: [
+					{
+						Id: 2,
+						Name: "Math",
+						Description: null
+					}
+				]
+			}
+		]);
+	});
 });
 
 describe("Get user with non existing id", () => {
-  it("Should return null", async () => {
-    expect(await GetUserFromId(999)).toBeNull();
-  })
-})
+	it("Should return null", async () => {
+		expect(await GetUserFromId(999)).toBeUndefined();
+	});
+});
