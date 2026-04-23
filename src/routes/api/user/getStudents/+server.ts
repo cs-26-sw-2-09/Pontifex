84ce494b7cf5807209a127f84c0a2d7129bf7954
Nicolai Holmgaard <Nicolai@Holmgaard.info>
2026-04-23 10:29:46 +0200

    Added database test These test the functions that are in the db/index.ts file

--- src/routes/api/user/getStudents/+server.ts
+++ src/routes/api/user/getStudents/+server.ts
@@ -5,29 +5,21 @@
 
 //initial commit
 
-import { db } from "$lib/server/db";
+import { db, GetUsersWithRole } from "$lib/server/db";
 import { Role } from "$lib/types";
 
 export async function GET() {
-	// Insert of test Data,
-	//const { Users } = await import("$lib/index");
+  // Insert of test Data,
+  //const { Users } = await import("$lib/index");
 
-	// Filters through users who has the student role, then maps the Id to an array of numbers
-	//const students: number[] = Users.filter((user) => user.Role === "Student").map(
-	//	(student) => student.Id
-	//);
-	const students = await db.query.User.findMany({
-		where: {
-			Role: Role.Student
-		},
-		columns: {
-			Id: true,
-			Name: true
-		}
-	});
+  // Filters through users who has the student role, then maps the Id to an array of numbers
+  //const students: number[] = Users.filter((user) => user.Role === "Student").map(
+  //	(student) => student.Id
+  //);
+  const students = await GetUsersWithRole(Role.Student);
 
-	// Retuns Json object including student users ID
-	return new Response(JSON.stringify(students), {
-		headers: { "Content-Type": "application/json" }
-	});
+  // Retuns Json object including student users ID
+  return new Response(JSON.stringify(students), {
+    headers: { "Content-Type": "application/json" }
+  });
 }
--- src/routes/api/user/getTeachers/+server.ts
+++ src/routes/api/user/getTeachers/+server.ts
@@ -3,30 +3,22 @@
 // Get all teacher ids
 // return all ids as an array
 
-import { db } from "$lib/server/db";
+import { db, GetUsersWithRole } from "$lib/server/db";
 import { Role } from "$lib/types";
 
 export async function GET() {
-	// Insert of test DB,
-	// const { Users } = await import("$lib/index");
+  // Insert of test DB,
+  // const { Users } = await import("$lib/index");
 
-	// Filters through users who has the teacher role, then maps the Id to an array of numbers
-	//const teachers: number[] = Users.filter((user) => user.Role === "Teacher").map(
-	//	(teacher) => teacher.Id
-	//);
+  // Filters through users who has the teacher role, then maps the Id to an array of numbers
+  //const teachers: number[] = Users.filter((user) => user.Role === "Teacher").map(
+  //	(teacher) => teacher.Id
+  //);
 
-	const teachers = await db.query.User.findMany({
-		where: {
-			Role: Role.Teacher
-		},
-		columns: {
-			Id: true,
-			Name: true
-		}
-	});
+  const teachers = await GetUsersWithRole(Role.Teacher);
 
-	// Retuns Json object including student users ID
-	return new Response(JSON.stringify(teachers), {
-		headers: { "Content-Type": "application/json" }
-	});
+  // Retuns Json object including student users ID
+  return new Response(JSON.stringify(teachers), {
+    headers: { "Content-Type": "application/json" }
+  });
 }
