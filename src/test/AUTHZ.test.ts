7f6e0f1b22dcc06f10406a76d15ea31e3a822aea
Rasmkrog <Rasmkrog@gmail.com>
2026-04-23 15:44:59 +0200

    Basetest for succes in PR

--- src/test/AUTHZ.test.ts
+++ src/test/AUTHZ.test.ts
@@ -14,3 +14,9 @@ import * as db from "$lib/server/db";
 // 		expect(authZ.hasAccess(adminUser, actions.Read, profileResource)).toBe(true);
 // 	});
 // });
+//base test for succes in PR
+describe("sum test", () => {
+	it("adds 1 + 2 to equal 3", () => {
+		expect(1 + 2).toBe(3);
+	});
+});
