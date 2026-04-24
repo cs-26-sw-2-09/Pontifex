585dcd4486da5f8a6a4cfe58eacb2d8ae00570c7
Krog <Rasmkrog@gmail.com>
2026-04-24 14:12:29 +0200

    removed unused function and imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,16 +3,9 @@
 	import favicon from "$lib/assets/favicon.svg";
 	import Navbar from "$lib/Components/Navbar.svelte";
 	import "@tailwindplus/elements";
-  import { resolve } from "$app/paths";
-	import { goto } from "$app/navigation";
-	import { deleteUserCookie } from "$lib/cookies/cookies";
 	let { children } = $props();
 	let cookie = 1;
-	
-  function logout() {
-		deleteUserCookie();
-		goto(resolve("/"));
-	}
+
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
