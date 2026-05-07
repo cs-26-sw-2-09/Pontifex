7a04ed1bcb77791ff70fc8d96a7239da2d75b1ff
Jeppe Duedahl Jensen <jdj10705@gmail.com>
2026-05-07 12:33:42 +0200

    Student assignments show up + some comments

--- src/routes/assignments/+page.server.ts
+++ src/routes/assignments/+page.server.ts
@@ -1,7 +1,7 @@
-import { db, GetUserFromId } from "$lib/server/db";
+import { db, GetUserFromId, GetCoursesFromUserId } from "$lib/server/db";
 import type { PageServerLoad } from "./$types";
 import { redirect } from "@sveltejs/kit";
-import type { Assignments } from "$lib/types"
+import type { Assignments } from "$lib/types";
 
 // Copy from other +pager.server.ts
 export const load: PageServerLoad = async ({ cookies }) => {
@@ -9,28 +9,34 @@ export const load: PageServerLoad = async ({ cookies }) => {
 	const userId = cookies.get("user");
 	if (!userId) return redirect(303, "/");
 
-	const user = await GetUserFromId(Number(userId)); // Callls GetUserFromId with the userId to get full user info from db and convert cooki e string to Number and if no user found redirect to homepage
+	const user = await GetUserFromId(Number(userId)); // Calls GetUserFromId with the userId to get full user info from db and convert cooki e string to Number and if no user found redirect to homepage
 	if (!user) return redirect(303, "/");
 
 	let assignments: Assignments[] = [];
 
 	if (user.Role === "Admin") {
+		// Admins see all assigments
 		assignments = await db.query.Assignments.findMany();
 	} else if (user.Role === "Teacher") {
+		// Teachers  see assighments they are responsible for
 		assignments = await db.query.Assignments.findMany({
 			where: { TeacherId: user.Id }
 		});
-	} /*else if (user.Role === "Student") {
-		
-	Students need to see assignments from the courses they are enrolled in (maybe importUserToCourses from schema?)
-	They also need to see if they have handedin an assignments (need to import HandedInAssignments from schema)
+	} else if (user.Role === "Student") {
+		// Students see assignments from courses they are enrolled in
+		const courses = await GetCoursesFromUserId(user.Id);
 
-	Need to get users courseIds to get all assignments for those courses
-	Then need to check if that assignment had been handedin
-	}*/
+		// Extract course IDs for assigments
+		const courseIds = courses.map(course => course.Id);
+		
+		// Get assignments related to the students courses
+		assignments = await db.query.Assignments.findMany({
+			where: { CourseId: { in: courseIds}}
+		});
+	}
+	
 	return {
-		//Need to send user info and assignments to svelte to display
 		user,
-		assignments //Svelte doesn't know what type assignments is for some reason, so this says it is of type Assignment
+		assignments
 	};
 };
