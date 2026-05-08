3bb34c6511e49ca29e019c8a258c852a6af16e6c
Kr0gZ <Rasmkrog@gmail.com>
2026-05-08 08:27:58 +0200

    Fix small css issue

--- src/routes/course/+page.svelte
+++ src/routes/course/+page.svelte
@@ -6,12 +6,12 @@
 
 <Navbar role={data.user!.Role} />
 
-<h1 class="text-center text-[40px] font-bold">These are your courses</h1>
+<h1 class="text-center text-4xl font-bold text-gray-100">These are your courses</h1>
 
 <div class="">
 	<ul class="box-border bg-gray-700">
 		{#each data.courses as course (course.Id)}
-			<li class="border-10 p-2 text-center text-[25px] text-white">{course.Name}</li>
+			<li class="border-10 p-2 text-center text-lg text-white">{course.Name}</li>
 		{/each}
 	</ul>
 </div>
