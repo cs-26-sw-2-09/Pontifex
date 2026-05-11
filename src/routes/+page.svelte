ba4672a0d9b874f1765bd0ba892051485d761574
Kr0gZ <Rasmkrog@gmail.com>
2026-05-11 17:38:59 +0200

    Refactor layout and styling for user role selection forms

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,18 @@
 	let { data } = $props();
 </script>
 
-<div class="flex min-h-screen flex-col items-center pt-48 text-center">
-	<h1 class="text-5xl font-bold text-gray-100">Lectio Pontifex</h1>
-	<h2 class="mt-4 text-xl text-gray-200">Who are you?</h2>
+<div class="flex h-screen flex-col items-center justify-center gap-10 text-center">
+	<div>
+		<h1 class="text-5xl font-bold text-gray-100">Lectio Pontifex</h1>
+		<h2 class="text-2xl font-light text-gray-200">Who are you?</h2>
+	</div>
 
-	<div class="mt-10 flex gap-6">
+	<div class="flex gap-6">
 		<form method="POST" action="?/login" class="flex flex-col gap-2">
 			<p class="text-center text-sm font-medium text-blue-400">Student</p>
 			<select
 				name="id"
-				class="rounded-xl border border-blue-400 bg-white px-6 py-3 text-blue-400 shadow-md"
+				class="appearance-none justify-between rounded-xl border border-blue-400 bg-gray-800 px-8 py-3 text-blue-400 shadow-md focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 focus:ring-offset-gray-900 focus:outline-none"
 			>
 				{#each data.students as student (student.Id)}
 					<option value={student.Id}>{student.Name}</option>
@@ -30,10 +32,13 @@
 			<p class="text-center text-sm font-medium text-emerald-400">Teacher</p>
 			<select
 				name="id"
-				class="rounded-xl border border-emerald-400 bg-white px-6 py-3 text-emerald-400 shadow-md"
+				class="appearance-none rounded-xl border border-emerald-400 bg-gray-800 px-8 py-3 text-emerald-400 shadow-md focus:ring-2 focus:ring-emerald-400 focus:ring-offset-2 focus:ring-offset-gray-900 focus:outline-none"
 			>
 				{#each data.teachers as teacher (teacher.Id)}
-					<option value={teacher.Id}>{teacher.Name}</option>
+					<option
+						class="flex justify-center gap-5 rounded-lg p-4 selection:bg-emerald-500"
+						value={teacher.Id}>{teacher.Name}</option
+					>
 				{/each}
 			</select>
 			<input type="hidden" name="role" value="Teacher" />
@@ -49,7 +54,7 @@
 			<p class="text-center text-sm font-medium text-red-400">Admin</p>
 			<select
 				name="id"
-				class="rounded-xl border border-red-400 bg-white px-6 py-3 text-red-400 shadow-md"
+				class="appearance-none rounded-xl border border-red-400 bg-gray-800 px-8 py-3 text-red-400 shadow-md focus:ring-2 focus:ring-red-400 focus:ring-offset-2 focus:ring-offset-gray-900 focus:outline-none"
 			>
 				{#each data.admins as admin (admin.Id)}
 					<option value={admin.Id}>{admin.Name}</option>
