16c827c958160d5fb93155bf34182f21389ac8f1
Kr0gZ <Rasmkrog@gmail.com>
2026-05-11 16:11:31 +0200

    Removed margin bottom 6

--- src/lib/Components/Navbar.svelte
+++ src/lib/Components/Navbar.svelte
@@ -11,7 +11,7 @@
 
 <!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
 <nav
-	class="relative mb-6 bg-gray-800 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10"
+	class="relative bg-gray-800 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10"
 >
 	<div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
 		<div class="relative flex h-16 items-center justify-between">
@@ -58,7 +58,7 @@
 					<div class="flex space-x-4">
 						<!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
 						<a
-							href={resolve(role ? `/${role.toLowerCase()}` : '/')}
+							href={resolve(role ? `/${role.toLowerCase()}` : "/")}
 							aria-current="page"
 							class="rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white">Home</a
 						>
@@ -130,7 +130,7 @@
 		<div class="space-y-1 px-2 pt-2 pb-3">
 			<!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
 			<a
-				href={resolve(role ? `/${role.toLowerCase()}` : '/')}
+				href={resolve(role ? `/${role.toLowerCase()}` : "/")}
 				aria-current="page"
 				class="block rounded-md bg-gray-950/50 px-3 py-2 text-base font-medium text-white">Home</a
 			>
