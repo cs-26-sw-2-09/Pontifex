c0c020a33a45419c54399d8817b289b70aa38219
Nicolai Holmgaard <Nicolai@Holmgaard.info>
2026-05-13 18:49:22 +0200

    Start of ACM

--- src/routes/assignments/[id]/+page.server.ts
+++ src/routes/assignments/[id]/+page.server.ts
@@ -2,81 +2,85 @@ import type { PageServerLoad } from "./$types";
 import { db, GetUserFromId } from "$lib/server/db";
 import { redirect } from "@sveltejs/kit";
 import { Review, Submissions } from "$lib/server/db/schema";
+import { HasAccessToAssignment } from "$lib/ACM/ReBAC.js";
+import { Actions } from "$lib/types.js";
+import { error } from '@sveltejs/kit';
+
 
 export const load: PageServerLoad = async ({ cookies, params }) => {
-	const userId: number = Number(cookies.get("user"));
+  const userId: number = Number(cookies.get("user"));
 
-	if (!userId) {
-		redirect(303, "/");
-	}
-	const user = await GetUserFromId(userId);
-	if (!user) {
-		redirect(303, "/");
-	}
-	if (user.Role === "Student") {
-		const assignment = await db.query.Assignments.findFirst({
-			where: { Id: Number(params.id) },
-			with: {
-				Submissions: {
-					where: {
-						UserId: userId
-					}
-				}
-			}
-		});
+  if (!userId) {
+    redirect(303, "/");
+  }
+  const user = await GetUserFromId(userId);
+  if (!user) {
+    redirect(303, "/");
+  }
+  if (user.Role === "Student") {
+    const assignment = await db.query.Assignments.findFirst({
+      where: { Id: Number(params.id) },
+      with: {
+        Submissions: {
+          where: {
+            UserId: userId
+          }
+        }
+      }
+    });
+    if (!await HasAccessToAssignment(user, Actions.Read, assignment)) throw error(403, "You do not have access to this assignment");
+    return { assignment, user };
+  } else {
+    const assignment = await db.query.Assignments.findFirst({
+      where: { Id: Number(params.id) },
+      with: {
+        Submissions: {
+          with: {
+            User: true
+          }
+        }
+      }
+    });
 
-		return { assignment, user };
-	} else {
-		const assignment = await db.query.Assignments.findFirst({
-			where: { Id: Number(params.id) },
-			with: {
-				Submissions: {
-					with: {
-						User: true
-					}
-				}
-			}
-		});
-
-		return { assignment, user };
-	}
+    return { assignment, user };
+  }
 };
 
 export const actions = {
-	submit: async ({ request, cookies, params }) => {
-		const userId: number = Number(cookies.get("user"));
-		const data = await request.formData();
-		const content = data.get("submission") as string;
+  submit: async ({ request, cookies, params }) => {
+    const userId: number = Number(cookies.get("user"));
+    const data = await request.formData();
+    const content = data.get("submission") as string;
 
-		if (!userId) {
-			redirect(303, "/");
-		}
+    if (!userId) {
+      redirect(303, "/");
+    }
 
-		await db.insert(Submissions).values({
-			Id: undefined,
-			AssignmentText: content,
-			UserId: userId,
-			AssignmentId: Number(params.id),
-			SubmissionDate: new Date()
-		});
-		redirect(303, `/assignments/${params.id}`);
-	},
-	grade: async ({ request, cookies, params }) => {
-		const userId: number = Number(cookies.get("user"));
-		const data = await request.formData();
-		const grade = Number(data.get("grade"));
-		const feedback = data.get("feedback") as string;
+    await db.insert(Submissions).values({
+      Id: undefined,
+      AssignmentText: content,
+      UserId: userId,
+      AssignmentId: Number(params.id),
+      SubmissionDate: new Date()
+    });
+    redirect(303, `/assignments/${params.id}`);
+  },
+  grade: async ({ request, cookies, params }) => {
+    const userId: number = Number(cookies.get("user"));
+    const data = await request.formData();
+    const grade = Number(data.get("grade"));
+    const feedback = data.get("feedback") as string;
 
-		if (!userId) {
-			redirect(303, "/");
-		}
+    if (!userId) {
+      redirect(303, "/");
+    }
 
-		await db.insert(Review).values({
-			SubmissionsId: Number(params.id),
-			Grade: grade,
-			TeacherId: userId,
-			Feedback: feedback
-		});
-		redirect(303, `/assignments/${params.id}`);
-	}
+    await db.insert(Review).values({
+      SubmissionsId: Number(params.id),
+      Grade: grade,
+      TeacherId: userId,
+      Feedback: feedback
+    });
+    redirect(303, `/assignments/${params.id}`);
+  }
 };
--- src/routes/course/[id]/+page.server.ts
+++ src/routes/course/[id]/+page.server.ts
@@ -1,13 +1,16 @@
 import type { PageServerLoad } from "./$types";
 import { GetCourseFromId, GetUserFromId } from "$lib/server/db";
-import { redirect } from "@sveltejs/kit";
+import { error, redirect } from "@sveltejs/kit";
+import { HasAccessToCourse } from "$lib/ACM/ReBAC";
+import { Actions } from "$lib/types";
 
 export const load: PageServerLoad = async ({ cookies, params }) => {
-	const userId: number = Number(cookies.get("user"));
-	if (!userId) {
-		redirect(303, "/");
-	}
-	const course = await GetCourseFromId(Number(params.id), true);
-	const user = await GetUserFromId(userId, true);
-	return { course, user };
+  const userId: number = Number(cookies.get("user"));
+  if (!userId) {
+    redirect(303, "/");
+  }
+  const course = await GetCourseFromId(Number(params.id), true);
+  const user = await GetUserFromId(userId, true);
+  if (!await HasAccessToCourse(user, Actions.Read, course)) throw error(403, "You do not have access to this course");
+  return { course, user };
 };
