02ac4e2e41590a7a9ab0a874f3dde65e17daaf1e
Nicolai Holmgaard <Nicolai@Holmgaard.info>
2026-05-15 11:59:54 +0200

    Fixed linting

--- src/routes/course/[id]/+page.server.ts
+++ src/routes/course/[id]/+page.server.ts
@@ -5,12 +5,13 @@ import { HasAccessToCourse } from "$lib/ACM/ReBAC";
 import { Actions } from "$lib/types";
 
 export const load: PageServerLoad = async ({ cookies, params }) => {
-  const userId: number = Number(cookies.get("user"));
-  if (!userId) {
-    redirect(303, "/");
-  }
-  const course = await GetCourseFromId(Number(params.id), true);
-  const user = await GetUserFromId(userId, true);
-  if (!await HasAccessToCourse(user, Actions.Read, course)) throw error(403, "You do not have access to this course");
-  return { course, user };
+	const userId: number = Number(cookies.get("user"));
+	if (!userId) {
+		redirect(303, "/");
+	}
+	const course = await GetCourseFromId(Number(params.id), true);
+	const user = await GetUserFromId(userId, true);
+	if (!(await HasAccessToCourse(user, Actions.Read, course)))
+		throw error(403, "You do not have access to this course");
+	return { course, user };
 };
